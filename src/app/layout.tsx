7414f454a0b63f71dcccb3cf0655272d5a74e4d2
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-03-23 22:02:34 -0300

    feat: page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Sidebar } from '@/components/Sidebar'
+import { Header } from '@/components/Header'
+import { HeroPattern } from '@/components/HeroPattern'
 
 const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
@@ -14,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className='bg-zinc-900'>{children}</body>
+      <body className="bg-zinc-900">
+        <Sidebar />
+        <div className="ml-80 relative h-screen">
+          <Header />
+          {/* <HeroPattern /> */}
+          <div className="py-24 max-w-4xl px-8 mx-auto">{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
